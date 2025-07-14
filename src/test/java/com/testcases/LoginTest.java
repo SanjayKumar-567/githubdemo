77aaac7f91b9ab3c3689081d02b8d053c9ad0a4e
SanjayKumar-567 <Sanjay97official@gmail.com>
2025-07-14 17:23:58 +0530

    second commit added one txt file and updated loginTest

--- src/test/java/com/testcases/LoginTest.java
+++ src/test/java/com/testcases/LoginTest.java
@@ -1,5 +1,10 @@
 package com.testcases;
 
 public class LoginTest {
+	public void logintest() {
+		System.out.println("this is logintest");
+		System.out.println("all  cases are passed");
+	}
+	
 
 }
